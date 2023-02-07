1d9d831df913ed3c84cd639f6de4e9b761b09e02
Rushi_Gandhi <rushigandhi1109@gmail.com>
2023-02-07 19:37:12 +0530

    removed unused imports

--- src/DigitBtn.js
+++ src/DigitBtn.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { ACTIONS } from './App'
 
 export default function digitBtn({ dispatch, digit }) {
--- src/OperationBtn.js
+++ src/OperationBtn.js
@@ -1,7 +1,5 @@
 import { ACTIONS } from './App'
 
-import React from 'react'
-
 export default function OperationBtn({ dispatch, operation }) {
   return (
     <button onClick={() => dispatch({ type:ACTIONS.CHOOSE_OPERATION, payload: {operation} })}>{operation}</button>
